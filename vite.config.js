db357ff22b8ba8ad8f8b34849c80dcffa9f06dcf
Tobi Bakare <tobibakz@gmail.com>
2024-11-28 22:04:18 +0100

    fix navbar component

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vercel from 'vite-plugin-vercel';
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-   port: process.env.PORT,
- },
- //plugins: [vercel()],
- resolve: {
-   alias: {
-     "@": path.resolve(__dirname, "./src"),
-   },
- },
+   server: {
+    port: process.env.PORT,
+  },
+  //plugins: [vercel()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
